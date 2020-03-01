ed491a8af8aeff42c3b125e5287ad33097911acc
sukesh-hg <sukesh.hg@niveussolutions.in>
2020-03-01 13:29:53 +0000

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('App received a request.');
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+const port = process.env.PORT || 8080;
+app.use('/', router);
+app.listen(port, () => {
+  console.log('App listening on port', port);
+});
